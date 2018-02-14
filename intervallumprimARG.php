10caba440c52833410e051dc147d29536cc3bd5b
horakdani <horak.daniel@gmail.com>
2018-02-14 15:47:51 +0100

    15:47

---
+++ intervallumprimARG.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($argc > 1) {
+        $n = $argv[1];
+        $m = $argv[2];
+
+        for ($i = $n; $i <= $m; $i++) {
+            $vanoszto = FALSE;
+            $oszto = 2;
+            while ($oszto < $i && $vanoszto !== TRUE) {
+                if ($i % $oszto == 0) {
+                    $vanoszto = TRUE;
+                }
+            $oszto++;
+            }
+            if ($vanoszto !== TRUE) {
+                echo $i . "\n";
+            } 
+        }
+}
+
