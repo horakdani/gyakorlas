be2a1c86c1ed50b6525647046a59101ebb0984da
horakdani <horak.daniel@gmail.com>
2018-02-21 12:05:30 +0100

    Az összeghasonlítást még meg kell oldanom!

--- muzeum2.php
+++ muzeum2.php
@@ -2,7 +2,7 @@
 
 $day = [];
 $visitors = [];
-
+$dayMin = 0;
 echo "Add meg a nap sorszámát és a látogatószámot\n";
 
 do {
@@ -11,20 +11,23 @@ do {
     array_push($visitors, $v);
 } while ($d !== 0 && $v !== 0);
 
+for ($i = 0; $i < count($day); $i++){
+    for ($x = $i+1; $x < count($day); $x++){
+        if ($day[$x] == $day[$i]){
+            $visitors[$i] = $visitors[$x] + $visitors[$i];
+            $visitors[$x] = 0;
+        }
+    }    
+}
+echo "ÚJ Napok tömb elemei:\n";
 print_r($day);
+echo "ÚJ Látogatók tömb elemei:\n";
 print_r($visitors);
 
-for ($i = 0; $i <= count($day); $i++){
-    for ($x = $i+1; $x <= count($day); $x++){
-        if ($day[$x] = $day[$i]){
-            $day[$i] = $visitors[$i] + $visitors[$x];
-            unset($day[$x]);
-            unset($visitors[$i]);
-            
-        }
+for ($i = 0; $i < count($visitors) - 1; $i++){
+    if ($visitors[$i + 1] < $visitors[$i] && $visitors[$i + 1] !== 0 ){
+        $dayMin = $day[$i + 1];
     }
-    
 }
 
-print_r($day);
-print_r($visitors);
+echo "\nA " . $day[$dayMin] . "-dik napon volt a legkevesebb látogató.";
