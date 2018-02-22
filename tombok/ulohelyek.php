676fc36b99529d8a26fc411c735968adfb9d6e5c
horakdani <horak.daniel@gmail.com>
2018-02-22 21:38:20 +0100

    Working and documented final version with random seat check

--- tombok/ulohelyek.php
+++ tombok/ulohelyek.php
@@ -5,24 +5,24 @@ $p = 180;
 $a = 0;
 $b = 0;
 
-// request seat data
+// data request  
 echo "\n";
 $persons = readline("Add meg, hányan szeretnétek egymás mellett ülni: ");
 echo "\n";
 
-// create an array with random elements to placeholders from 1 to 195
+// create an array with random elements to seats from 1 to 195
 $seatStatus = [];
 for ($i = 0; $i < 195; $i++) {
     array_push($seatStatus, rand(0, 1));
 }
 
-// array matrix
+// array matrix (display and calculate)
 for ($rows = 0; $rows < 13; $rows++) {
-    // display array
+    // display seat statuses in row
     for ($q = $p; $q < $p + 15; $q++) {
         echo sprintf("%4s", $seatStatus[$q]);
     }
-    // calculate free seats
+    // calculate free seats beside each other in row
     for ($q = $p; $q < $p + 15 - ($persons - 1); $q++) {
         for ($y = 0; $y < $persons; $y++) {
             $a = $a + $seatStatus[$q + $y];
@@ -43,26 +43,27 @@ $p = 180;
 
 echo "\n";
 
-// display seat numbers
+// display seat numbers (optional)
 $seatNumbers = [];
-echo $option = readline("Szeretnéd látni az ülőhelyek kiosztását? (i / n): ");
+
+echo $option = readline("Szeretnéd látni az ülőhelyek kiosztását? (i/n): ");
 
 if ($option == "i") {
     for ($i = 1; $i <= 195; $i++) {
         array_push($seatNumbers, $i);
     }
     echo "\n";
-    // display array elements
+    // display seat numbers
     for ($rows = 0; $rows < 13; $rows++) {
         for ($q = $p; $q < $p + 15; $q++) {
             echo sprintf("%4s", $seatNumbers[$q]);
         }
-        echo "\n";
         $p = $p - 15;
+        echo "\n";
     }
 } else {
     die();
 }
 
-// check array element are in the right placeholder of array
+// some seat status check if seat numbers are displayed
 echo "\nRandom check:\n\nseat #195: $seatStatus[194]\nseat #181: $seatStatus[180]\nseat #15:  $seatStatus[14]\n";
