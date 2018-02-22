c3ae2b1fd92ce957b99a3ac9300fd705fa8c9e61
horakdani <horak.daniel@gmail.com>
2018-02-22 15:25:52 +0100

    15:25

---
+++ strings/feluliras_csere.php
@@ -0,0 +1,34 @@
+<?php
+
+echo "\n";
+$str = readline("Add meg egy sztringet: ");
+echo "\n";
+$mit = readline("Add meg, melyik karaktert szeretnéd kicserélni a sztringben: ");
+echo "\n";
+$mire = readline("Add meg, mire szeretnéd ezt a karaktert kicserélni: ");
+echo "\n";
+
+echo charReplace($str, $mit, $mire);
+echo "\n";
+echo charTurn($str, $mit, $mire);
+
+//-----------function---------------------------------------------------------
+function charReplace($str, $mit, $mire) {
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] == $mit) {
+            $str[$i] = $mire;
+        } 
+    }
+    echo "Az eredmény: " . $str . "\n";
+}
+
+function charTurn($str, $mit, $mire) {
+    for ($i = 0; $i < strlen($str); $i++) {
+        if ($str[$i] == $mit) {
+            $str[$i] = $mire;
+        } elseif ($str[$i] == $mire) {
+            $str[$i] = $mit;            
+        }
+    }
+    echo "Az eredmény: " . $str . "\n";
+}
--- strings/sztring_muveletek.php
+++ strings/sztring_muveletek.php
@@ -2,16 +2,17 @@
 
 echo "\nALAPVETŐ SZTRING MŰVELETEK:\n\n";
 
-$string = " Lorem ipsum dolor sit amet, ius in fugit elaboraret. ";
+$string = " Lorem a ipsum, a dolor sit amet, ius. In a fugit elaboraret. ";
 
 echo "\n\$string:\t\t" . "\"" . $string . "\"\n\n";
 echo "strlen:\t\t\tA sztring " . strlen($string) . " karakter hosszú.\n\n";
 echo "strpos:\t\t\tAz \"u\" betű első előfordulása a sztringben a " . strpos($string, "u") . "-dik helyen van.\n\n";
-echo "substr:\t\t\t" .substr($string, 7, 5) . "\n\n";
-echo "str_replace:\t\t" .str_replace("o", "OO", $string) . "\n\n";
+echo "substr:\t\t\t" . substr($string, 7, 5) . "\n\n";
+echo "str_replace:\t\t" . str_replace("o", "OO", $string) . "\n\n";
 echo "strtoupper:\t\t" . strtoupper($string) . "\n\n";
 echo "trim:\t\t\t" . trim($string) . "\n\n";
 echo "ltrim:\t\t\t" . ltrim($string) . "\n\n";
+echo "substr_count:\t\t Az \"or\" karakterlánc " . substr_count($string, "or") . "-szor fordul elő a sztingben\n\n";
 
 echo "explode:\t\t";
 $str = explode(" ", trim($string));
@@ -24,9 +25,15 @@ echo "implode:\t\t";
 echo implode(" ", $str);
 echo "\n\n";
 
+echo "LOW CHAR NUM FUNC:\tA sztringben " . lowerCharNum($string) . "db kisbetűs karakter található.\n\n";
 
-
-
-
-
-
+//------------functions-------------------------------
+function lowerCharNum($string) {
+    $counter = 0;
+    for ($i = 0; $i < strlen($string); $i++) {
+        if (ctype_lower($string[$i]) === true) {
+            $counter++;
+        }
+    }
+    return $counter;
+}
