d4e416b005c5d6a620b4355cc529476f7db7836e
horakdani <horak.daniel@gmail.com>
2018-02-22 19:51:29 +0100

    ömlesztett

--- strings/sztring_muveletek.php
+++ strings/sztring_muveletek.php
@@ -2,13 +2,15 @@
 
 echo "\nALAPVETŐ SZTRING MŰVELETEK:\n\n";
 
-$string = " Lorem a ipsum, a dolor sit amet, ius. In a fugit elaboraret. ";
+$string = readline("Írj/illessz be szöveget: ");
 
-echo "\n\$string:\t\t" . "\"" . $string . "\"\n\n";
+echo "\n-----------------------------------------------------------------------------------------------------------------\n";
+echo "\$string:\t\t" . "\"" . $string . "\"";
+echo "\n-----------------------------------------------------------------------------------------------------------------\n\n";
 echo "strlen:\t\t\tA sztring " . strlen($string) . " karakter hosszú.\n\n";
 echo "strpos:\t\t\tAz \"u\" betű első előfordulása a sztringben a " . strpos($string, "u") . "-dik helyen van.\n\n";
 echo "substr:\t\t\t" . substr($string, 7, 5) . "\n\n";
-echo "str_replace:\t\t" . str_replace("o", "OO", $string) . "\n\n";
+echo "str_replace:\t\t" . str_replace(" ", "", $string) . "\n\n";
 echo "strtoupper:\t\t" . strtoupper($string) . "\n\n";
 echo "trim:\t\t\t" . trim($string) . "\n\n";
 echo "ltrim:\t\t\t" . ltrim($string) . "\n\n";
