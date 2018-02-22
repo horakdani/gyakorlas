f928ea1cd4b26a0be7bd60995b00ad5f87d68d5f
horakdani <horak.daniel@gmail.com>
2018-02-22 21:23:27 +0100

    Working and documented final version with random seat check

--- tombok/ulohelyek.php
+++ tombok/ulohelyek.php
@@ -1,7 +1,7 @@
 <?php
 
 // variables
-$p = 0;
+$p = 180;
 $a = 0;
 $b = 0;
 
@@ -10,25 +10,22 @@ echo "\n";
 $persons = readline("Add meg, hányan szeretnétek egymás mellett ülni: ");
 echo "\n";
 
-// create an array with random elements from 1-195
-$array = [];
+// create an array with random elements to placeholders from 1 to 195
+$seatStatus = [];
 for ($i = 0; $i < 195; $i++) {
-    array_push($array, rand(0, 1));
+    array_push($seatStatus, rand(0, 1));
 }
-
-
 
 // array matrix
 for ($rows = 0; $rows < 13; $rows++) {
-    // display array elements
-    for ($col = 0; $col < 15; $col++) {
-        echo sprintf("%4s", $array[$p]);
-        $p++;
+    // display array
+    for ($q = $p; $q < $p + 15; $q++) {
+        echo sprintf("%4s", $seatStatus[$q]);
     }
-    // calculaet free seats beside each other
-    for ($x = $p - 15; $x < $p - ($persons - 1); $x++) {
+    // calculate free seats
+    for ($q = $p; $q < $p + 15 - ($persons - 1); $q++) {
         for ($y = 0; $y < $persons; $y++) {
-            $a = $a + $array[$x + $y];
+            $a = $a + $seatStatus[$q + $y];
         }
         if ($a == 0) {
             $b++;
@@ -39,35 +36,33 @@ for ($rows = 0; $rows < 13; $rows++) {
         echo "    OK";
     }
     $b = 0;
+    $p = $p - 15;
     echo "\n";
 }
+$p = 180;
+
 echo "\n";
 
-
-
-$p = 180;
-$array2 = [];
+// display seat numbers
+$seatNumbers = [];
 echo $option = readline("Szeretnéd látni az ülőhelyek kiosztását? (i / n): ");
 
-if ($option = "i") {
+if ($option == "i") {
     for ($i = 1; $i <= 195; $i++) {
-        array_push($array2, $i);
+        array_push($seatNumbers, $i);
     }
     echo "\n";
     // display array elements
     for ($rows = 0; $rows < 13; $rows++) {
-        // display array elements
-
         for ($q = $p; $q < $p + 15; $q++) {
-
-            echo sprintf("%4s", $array2[$q]);
+            echo sprintf("%4s", $seatNumbers[$q]);
         }
         echo "\n";
         $p = $p - 15;
     }
+} else {
+    die();
 }
 
-
-
-
-
+// check array element are in the right placeholder of array
+echo "\nRandom check:\n\nseat #195: $seatStatus[194]\nseat #181: $seatStatus[180]\nseat #15:  $seatStatus[14]\n";
