196a6e6486b7f83166ae1f8f88874bd0e23ae794
horakdani <horak.daniel@gmail.com>
2018-02-23 13:46:15 +0100

    kész

---
+++ strings/bevezeto_lezaro.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "\n";
+$str = readline("Adj meg egy szöveget: ");
+echo "\n";
+$chr = readline("Add meg a feltöltő karaktert: ");
+echo "\n";
+$num = readline("Add meg a feltöltő karakter darabszámát: ");
+echo "\n";
+
+bevLez($str, $chr, $num);
+
+//--------------function-----------------------
+function bevLez($str, $chr, $num) {
+    $strNew;
+    $chrNum = "";
+    for ($i = 0; $i < $num; $i++) {
+        $chrNum .= $chr;
+    }
+    $strNew = $chrNum . $str . $chrNum;
+    $strNew = str_replace(" ", $chr, $strNew);
+    echo $strNew;
+    echo "\n";
+}
