d3a5a724c642c0d7d0a2e4cca91d786d7703cd1c
horakdani <horak.daniel@gmail.com>
2018-02-26 13:04:12 +0100

    v1.2

--- idomero/idomero.php
+++ idomero/idomero.php
@@ -1,5 +1,6 @@
 <?php
 
+// Date check. Are we started the day today?
 $date = new DateTime();
 $date = $date->format("20y.m.d. D");
 
@@ -8,16 +9,27 @@ $isDate = true;
 $fp = fopen("ugymenetek", "r");
 
 while (($lines = fgets($fp)) !== false) {
-    if ($lines === $date){
-        $isDate = false;        
+    if ($lines === $date . "\n") {
+        $isDate = false;
     }
+    echo $isDate;
 }
 
 fclose($fp);
 
-if ($isDate){
+if ($isDate) {
     $current = file_get_contents("ugymenetek");
     $current .= $date . "\n";
     file_put_contents("ugymenetek", $current);
+} else {
+    $current = file_get_contents("ugymenetek");
+    file_put_contents("ugymenetek", $current);
     
+}
+
+// Add case and timestap to storage file
+if ($argc === 2) {
+    $current = file_get_contents("ugymenetek");
+    $current .= $argv[1] . "\t" . time() . "\n";
+    file_put_contents("ugymenetek", $current);
 }
--- idomero/test.php
+++ idomero/test.php
@@ -3,13 +3,6 @@
 //2018.02.25. Sun
 //2018.02.26. Mon
 
-
-$file = "ugymenetek";
-// Open the file to get existing content
-$current = file_get_contents($file);
-// Append a new person to the file
-$current .= "John Smith\n";
-// Write the contents back to the file
-file_put_contents($file, $current);
+echo time();
 
 
