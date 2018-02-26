299ec7b864b78466b29021b4010444005c916e69
horakdani <horak.daniel@gmail.com>
2018-02-26 13:06:55 +0100

    v1.3

--- idomero/idomero.php
+++ idomero/idomero.php
@@ -16,18 +16,14 @@ while (($lines = fgets($fp)) !== false) {
 }
 
 fclose($fp);
-
+// Add date if it is a new day
 if ($isDate) {
     $current = file_get_contents("ugymenetek");
     $current .= $date . "\n";
     file_put_contents("ugymenetek", $current);
-} else {
-    $current = file_get_contents("ugymenetek");
-    file_put_contents("ugymenetek", $current);
-    
-}
+} 
 
-// Add case and timestap to storage file
+// Add case and timestap to storage file from argument
 if ($argc === 2) {
     $current = file_get_contents("ugymenetek");
     $current .= $argv[1] . "\t" . time() . "\n";
