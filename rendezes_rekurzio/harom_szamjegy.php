e410d1fd81fd35f7e4e66246348b68f3896cf1e2
horakdani <horak.daniel@gmail.com>
2018-02-28 21:16:15 +0100

    kész

--- rendezes_rekurzio/harom_szamjegy.php
+++ rendezes_rekurzio/harom_szamjegy.php
@@ -2,16 +2,15 @@
 
 //Írj függvényt, amely a paraméterként kapott pozitív egész számot három számjegyenként
 //csoportosított formában írja ki. Pl.: 16 077 216. Próbáld ki más számokra is: 999, 1000, 12, 0, 1000222!
-
+echo "\n";
 $num = readline("Adj meg egy számot: ");
 
-haromSzamjegy($num);
+echo "\n" . haromSzamjegy($num) . "\n";
 
-function haromSzamjegy($num){
-    if ($num / 1000 > 0){
-        echo $num;
+function haromSzamjegy($num) {
+    if ($num / 1000 < 1) {
+        return $num % 1000;
     } else {
-        haromSzamjegy($num / 100);
-        echo " " . ($num % 3) . " ";
+        return haromSzamjegy($num / 1000) . " " . ($num % 1000);
     }
 }
