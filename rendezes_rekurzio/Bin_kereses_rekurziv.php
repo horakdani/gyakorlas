9bd1dffb4ea66dbb49b80da0b0120dbbcbeb7915
horakdani <horak.daniel@gmail.com>
2018-03-02 12:57:12 +0100

    12:57

---
+++ rendezes_rekurzio/Bin_kereses_rekurziv.php
@@ -0,0 +1,21 @@
+<?php
+
+$array = [1, 3, 8, 23, 49, 115, 314];
+
+echo binSearch($array, 115, 0, count($array));
+
+function binSearch($array, $searchingValue, $arrayHead, $arrayEnd) {
+    $middle = floor(($arrayHead + $arrayEnd) / 2);
+    if ($arrayHead <= $arrayEnd && $array[$middle] != $searchingValue) {
+        if ($array[$middle] > $searchingValue) {
+            $arrayEnd = $middle - 1;
+        } else {
+            $arrayHead = $middle + 1;
+        }
+        return binSearch($array, $searchingValue, $arrayHead, $arrayEnd);
+    }
+    if ($arrayEnd < $arrayHead) {
+        return false;
+    }
+    return ($middle);
+}
---
+++ rendezes_rekurzio/rendezes_kozvetlen_kiv.php
@@ -0,0 +1,24 @@
+<?php
+
+$array = [65, 15, 98, 2, 84, 12, 116, 8, 23, 69, 116, 158, 42, 65, 1, 3, 2];
+
+sorter($array, 0, 1);
+
+foreach ($array as $value) {
+    echo $value . ", ";
+}
+
+
+function sorter($array, $i, $j) {
+    if ($i == count($array)) {
+        return;
+    } elseif ($array[$i + $j] < $array[$i] && $j < count($array)) {
+            $temp = $array[$i];
+            $array[$i] = $array[$i + $j];
+            $array[$i + $j] = $temp;
+            return sorter($array, $i + 1, $j);
+        } else {
+            return sorter($array, $i, $j + 1);
+        }
+        return;
+    }
